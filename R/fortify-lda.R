db5ffa20b0be74ac4223612732dd82c36b88d57b
Michael Friendly <friendly@yorku.ca>
2023-09-14 16:32:08 -0400

    initial attempt to install fortify methods

---
+++ R/fortify-lda.R
@@ -0,0 +1,57 @@
+#' Fortify method for linear discriminant analysis objects produced 
+#' by \code{lda} in \pkg{MASS}
+#' 
+#' @name fortify-lda
+#'
+#' @param model   an object of class \code{\link[MASS]{lda}}
+#' @param data    discriminant scores and class predictions are computed for data 
+#' @param scale   scale the scores and loadings as done by \code{\link[stats]{biplot.prcomp}}
+#' @param equalize should the basis vectors be rescaled to be comparable with the score vectors?
+#' @param ...     not used
+#'
+#' @importFrom ggplot2 fortify
+#' @return A data frame consisting of the columns of \code{data} together with 
+#'         discriminant scores and class predictions.  Additionally, the attribute 
+#'         \code{basis} is set to a data frame containing the loadings and a 
+#'         column of variable names if present in \code{model}
+#'
+#' @examples
+#' library(MASS)
+#' irislda <- lda(Species ~ ., data = iris)
+#' df <- fortify(irislda, iris)
+#' ggplot(df, aes(x = LD1, y = LD2)) +
+#'   geom_point(aes(group = Species, color = Species, shape = Species)) + 
+#'   stat_ellipse(aes(group = Species, color = Species)) +
+#' #  geom_axis(data = attr(df, "basis"), aes(label = abbreviate(.name))) + 
+#'   geom_point(data = fortify(irislda, data.frame(m$means)), shape = 3) + 
+#'   ylim(-4, 4) + coord_equal()
+#' @exportS3Method fortify lda
+fortify.lda <- function(model, data = NULL, scale = 0, equalize = scale != 0, ...) {
+  
+  # Predict
+  fit <- if (is.null(data)) {
+    predict(model, ...)
+  } else {
+    predict(model, data, ...)
+  }
+  
+  # Rescale
+  scores <- sweep(fit$x, 2, model$svd^(-scale), FUN = "*")
+  loadings <- sweep(model$scaling, 2, model$svd^scale, FUN = "*")
+  
+  if(equalize) {
+    r <- sqrt( median(rowSums(scores^2)) / max(colSums(loadings^2)) )
+    loadings <- loadings * r
+  }
+  
+  scores <- data.frame(scores, .class = fit$class)
+  loadings <- data.frame(loadings)
+  
+  if (!is.null(data)) scores <- cbind(data, scores)
+  if (!is.null(rownames(loadings))) {
+    loadings$.name <- rownames(loadings)
+    rownames(loadings) <- NULL
+  }
+  
+  structure(scores, basis = loadings)
+}
---
+++ R/fortify-pca.R
@@ -0,0 +1,85 @@
+#' Fortify methods for principal components analysis objects produced 
+#' by \code{prcomp}, \code{princomp}, and \code{PCA} in \pkg{FactoMineR}
+#' 
+#' @name fortify-pca
+#' @aliases fortify.prcomp fortify.princomp fortify.PCA
+#'
+#' @param model   an object of class \code{prcomp}, \code{princomp}, or 
+#'                \code{\link[FactoMineR]{PCA}}
+#' @param data    principal component scores are computed for data; not used 
+#'                for \code{\link[FactoMineR]{PCA}} objects 
+#' @param scale   scale the scores and loadings as done by \code{\link[stats]{biplot.prcomp}}
+#' @param equalize should the basis vectors be rescaled to be comparable with the score vectors?
+#' @param ...     not used
+#'
+#' @importFrom stats median
+#' @importFrom ggplot2 fortify
+#' @return A data frame consisting of the columns of \code{data} together with 
+#'         principal component scores.  The attribute "basis" is set to to a data frame containing the loadings and a 
+#'         column of variable names if present in \code{model}. The attribute "radius" is set to either 1 or the scale factor used to equalize the basis vectors.
+#'
+#' @examples
+#' data(wine)
+#' winepca <- prcomp(wine, scale = TRUE)
+#' cultivar <- wine.class
+#' df <- fortify(winepca, wine)
+#' ggplot(df, aes(x = PC1, y = PC2)) +
+#'   geom_point(aes(group = cultivar, color = cultivar)) + 
+#'   stat_ellipse(aes(group = cultivar, color = cultivar)) +
+#' #  geom_axis(data = attr(df, "basis"), aes(label = .name), scale = 3) +
+#'   coord_equal()
+NULL
+
+.fortify_pca <- function(scores, loadings, sdev, scale, equalize, data) {
+  
+  scores <- sweep(scores, 2, sdev^(-scale), FUN = "*")
+  loadings <- sweep(loadings, 2, sdev^(scale), FUN = "*")
+  radius <- 1
+  if(equalize) {
+    radius <- sqrt( median(rowSums(scores^2)) / max(colSums(loadings^2)) )
+    loadings <- loadings * radius
+  }
+  scores <- data.frame(scores)
+  loadings <- data.frame(loadings)
+  names(scores) <- paste("PC", seq_len(ncol(scores)), sep = "")
+  names(loadings) <- paste("PC", seq_len(ncol(loadings)), sep = "")
+  
+  if (!is.null(data)) scores <- cbind(data, scores)
+  if (!is.null(rownames(loadings))) {
+    loadings$.name <- rownames(loadings)
+    rownames(loadings) <- NULL
+  }
+  
+  structure(scores, 
+            basis = loadings, 
+            radius = radius)
+}
+
+#' @method fortify prcomp
+#' @rdname fortify-pca
+#' @exportS3Method fortify prcomp
+fortify.prcomp <- function(model, data = NULL, scale = 0, equalize = scale != 0, ...) {
+  scores <- if (is.null(data)) predict(model) else predict(model, data)
+  .fortify_pca(scores, model$rotation, model$sdev, scale, equalize, data)
+}
+
+#' @method fortify princomp
+#' @rdname fortify-pca
+#' @exportS3Method fortify princomp
+fortify.princomp <- function(model, data = NULL, scale = 0, equalize = scale != 0, ...) {
+  scores <- if (is.null(data)) predict(model) else predict(model, data)
+  loadings <- model$loadings
+  class(loadings) <- "matrix"
+  .fortify_pca(scores, loadings, model$sdev, scale, equalize, data)
+}
+
+#' @method fortify PCA
+#' @rdname fortify-pca
+#' @exportS3Method fortify PCA
+fortify.PCA <- function(model, data = NULL, scale = 0, equalize = scale != 0, ...) {
+  pcobj <- model     ## Is this a kludge?
+  scores <- data.frame(pcobj$ind$coord)
+  loadings <- data.frame(pcobj$var$coord)
+  sdev <- sqrt(pcobj$eig[1:ncol(pcobj$var$coord), 1])
+  .fortify_pca(scores, loadings, sdev, scale, equalize, data)
+}
